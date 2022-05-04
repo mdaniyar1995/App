4bdd1a14135fa2a9bbf151b7e90f7a862847cc3e
mambetkerimov <mdaniyar1995@gmail.com>
2022-05-04 16:02:12 +0300

    new

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -2,25 +2,25 @@ import java.util.Arrays;
 
 public class DynamicArray {
     private int size;
-    private String[] data;
+    private Object[] data;
 
     public DynamicArray() {
         this(20);
     }
 
     public DynamicArray(int initialCapacity) {
-        data = new String[initialCapacity];
+        data = new Object[initialCapacity];
         size = 0;
     }
 
-    public void add(String value) {
+    public void add(Object value) {
         if (size >= data.length) {
             data = grow(data);
         }
         data[size++] = value;
     }
 
-    private static String[] grow(String[] oldArray) {
+    private static Object[] grow(Object[] oldArray) {
 
         return Arrays.copyOf(oldArray, oldArray.length * 2);
     }
